c710f10c9a8a1afb5b4b55fe570bd4b5563c9e56
MSRAJUP <80346865+MSRAJUP@users.noreply.github.com>
2022-07-21 22:17:02 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 VERSION=$(date +%H-%M-%S)
 docker build -t msrajup/testingbuild:${VERSION} .
 docker push msrajup/testingbuild:${VERSION}
-docker -H tcp://10.1.1.200:2375 stop nginx
-docker -H tcp://10.1.1.200:2375 run --rm -dit -p 8000:80 --name nginx --hostname nginx msrajup/testingbuild:${VERSION}
+docker -H tcp://10.0.1.200:2375 stop nginx
+docker -H tcp://10.0.1.200:2375 run --rm -dit -p 8000:80 --name nginx --hostname nginx msrajup/testingbuild:${VERSION}
 echo "---------------------"
